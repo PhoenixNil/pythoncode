b54d19c97ad7922fd84a3af15dcd5733f30ae5f4
Zero <1270128439@qq.com>
2022-11-02 00:58:34 +0000

    test10

--- audio-analyse.py
+++ audio-analyse.py
@@ -32,33 +32,41 @@ def synthesize_to_speaker():
 
 
 synthesize_to_speaker()
-# def recognize_from_microphone():
-#     speech_translation_config = speechsdk.translation.SpeechTranslationConfig(subscription="32fcc79994d1428882b0ab32d2eec1c1", region="eastus")
-#     speech_translation_config.speech_recognition_language="en-US"
 
-#     target_language="zh-Hans"
-#     speech_translation_config.add_target_language(target_language)
 
-#     audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
-#     translation_recognizer = speechsdk.translation.TranslationRecognizer(translation_config=speech_translation_config, audio_config=audio_config)
+def recognize_from_microphone():
+    speech_translation_config = speechsdk.translation.SpeechTranslationConfig(
+        subscription="32fcc79994d1428882b0ab32d2eec1c1", region="eastus")
+    speech_translation_config.speech_recognition_language = "en-US"
 
-#     print("Speak into your microphone.")
-#     translation_recognition_result = translation_recognizer.recognize_once_async().get()
+    target_language = "zh-Hans"
+    speech_translation_config.add_target_language(target_language)
 
-#     if translation_recognition_result.reason == speechsdk.ResultReason.TranslatedSpeech:
-#         print("Recognized: {}".format(translation_recognition_result.text))
-#         print("""Translated into '{}': {}""".format(
-#             target_language,
-#             translation_recognition_result.translations[target_language]))
-#     elif translation_recognition_result.reason == speechsdk.ResultReason.NoMatch:
-#         print("No speech could be recognized: {}".format(translation_recognition_result.no_match_details))
-#     elif translation_recognition_result.reason == speechsdk.ResultReason.Canceled:
-#         cancellation_details = translation_recognition_result.cancellation_details
-#         print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-#         if cancellation_details.reason == speechsdk.CancellationReason.Error:
-#             print("Error details: {}".format(cancellation_details.error_details))
-#             print("Did you set the speech resource key and region values?")
+    audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
+    translation_recognizer = speechsdk.translation.TranslationRecognizer(
+        translation_config=speech_translation_config, audio_config=audio_config)
 
-# recognize_from_microphone()
+    print("Speak into your microphone.")
+    translation_recognition_result = translation_recognizer.recognize_once_async().get()
+
+    if translation_recognition_result.reason == speechsdk.ResultReason.TranslatedSpeech:
+        print("Recognized: {}".format(translation_recognition_result.text))
+        print("""Translated into '{}': {}""".format(
+            target_language,
+            translation_recognition_result.translations[target_language]))
+    elif translation_recognition_result.reason == speechsdk.ResultReason.NoMatch:
+        print("No speech could be recognized: {}".format(
+            translation_recognition_result.no_match_details))
+    elif translation_recognition_result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = translation_recognition_result.cancellation_details
+        print("Speech Recognition canceled: {}".format(
+            cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(
+                cancellation_details.error_details))
+            print("Did you set the speech resource key and region values?")
+
+
+recognize_from_microphone()
 
 # 语音翻译
