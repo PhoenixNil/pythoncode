4d9849be61148e587f14e468bd6d0653605d5ec6
Zero <1270128439@qq.com>
2023-06-09 16:22:44 +0100

    生成谷歌域名链接为了 安全搜索 设置在host里面

---
+++ redirgoogledomain.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+# 发送HTTP GET请求获取网页内容
+url = "https://www.fobnotes.com/tools/google-global-country-sites/"  
+url2= "https://zhuanlan.zhihu.com/p/83273024" 
+response = requests.get(url)
+response2 = requests.get(url2)
+googledomain=[]
+# 检查请求是否成功
+if response.status_code == 200:
+    # 使用BeautifulSoup解析网页内容
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # 找到所有的链接
+    links = soup.find_all("a")
+    for link in links :
+        if "google" in link.get("href")  and len(link.get("href"))<30:
+            googledomain.append(link.get("href"))
+else:
+    print("请求失败，错误代码:", response.status_code)
+
+if response2.status_code == 200:
+    soup = BeautifulSoup(response2.content, "html.parser")
+    class_element = soup.find(class_="RichText ztext Post-RichText css-1g0fqss")
+    # 找到所有的链接
+    if class_element:
+        # print(class_element.find_all("a"))
+        for link in class_element.find_all("a"):
+            googledomain.append(link.get("href").replace("https://link.zhihu.com/?target=https%3A//", ""))
+
+else:
+    print("请求失败，错误代码:", response.status_code)
+
+address_domin=["2001:4860:4802:32::78 "+ element[:-1].replace("https://", "") +" #forcesafesearch" for element in googledomain] #去掉最后的/
+sort_merge_address_domin= sorted(list(set(address_domin)))
+for i in sort_merge_address_domin:
+    print(i)
