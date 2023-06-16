3a5371fd78c2d230d44996d7256df8b6db8bae59
Zero <1270128439@qq.com>
2023-06-16 09:32:42 +0100

    修改

--- MScharactername.py
+++ MScharactername.py
@@ -1,2 +1,3 @@
-chs = u'魂骑士'
+chs = u'天'
 print(f"{chs.encode('936').decode('1252')}")
+# 秋，骑,天
--- dropout.py
+++
@@ -1,4 +0,0 @@
-from keras.models import Sequential
-from keras.layers import Dense
-model = Sequential()
-model.add(Dense(1, input_shape=(2,), activation='sigmoid'))
